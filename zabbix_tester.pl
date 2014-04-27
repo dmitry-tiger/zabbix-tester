4b1e97e0b87bd5cc944fa76767541892a992d577
Dmitry Tigrov <omo@bk.ru>
2014-04-28 03:37:25 +0400

    add timetable and queue maker

--- zabbix_tester.pl
+++ zabbix_tester.pl
@@ -1,5 +1,6 @@
 use AnyEvent::DBI::MySQL;
 # get cached but not in use $dbh
+my $cv = AnyEvent->condvar;
 my $database='zabbix_proxy';
 my $hostname='192.168.0.4';
 my $port='3306';
@@ -23,10 +24,10 @@ $dbh->selectrow_hashref($sel_countq,{async=>0},sub {
     for my $i  (0..int($hash_ref->{count}/$limit_rows_from_db)){
         my $sel_items='SELECT
             items.itemid as itemid,
-            items.key_ as key,
+            items.key_ as key_,
             items.delay as delay,
-            items.type,
-            items.status,
+            items.type as itype,
+            items.status as istatus,
             items.value_type as value_type,
             `hosts`.`status`,
             `hosts`.`host` as host
@@ -40,7 +41,33 @@ $dbh->selectrow_hashref($sel_countq,{async=>0},sub {
         $dbh->selectall_hashref($sel_items,'itemid',{async=>0},sub {
             my ($hash_ref) = @_;
            # $timetable
-            print scalar keys %{$hash_ref},"\n";
+            my $tshift=0;
+            foreach my $key (keys %{$hash_ref}){
+                push @{$timetable{time()+$tshift}},{itemid=>$hash_ref->{$key}->{itemid},
+                                                    key=>$hash_ref->{$key}->{key_},delay=>$hash_ref->{$key}->{delay},
+                                                    itype=>$hash_ref->{$key}->{itype},value_type=>$hash_ref->{$key}->{value_type}};
+                $tshift++;
+            }
             });
         };
     });
+
+my $t = AnyEvent->timer (
+    after => 0,
+    interval => 1,
+    cb => sub {
+        my $ctime = time();
+        if (exists $timetable{$ctime}) {
+            print "exists on ",$ctime,"\n";
+            foreach my $kk (@{$timetable{$ctime}}){
+               print $kk->{itemid}."\n";
+            }
+        }
+        
+        #print time(),"\n";
+    });
+        
+        
+        
+        
+$cv->recv;
